24616126a56cf0d07e952583a45463ecdf55431a
indi50909 <136218877+indi50909@users.noreply.github.com>
2026-05-21 23:15:43 +0800

    Add files via upload

---
+++ story_content/user.js
@@ -0,0 +1,19 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "64iab3zT34X":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  var audio = document.getElementByld('bgSongku');
+audio.src="musikki.mp3";
+audio.load():
+audio.play();
+audio.volume=0.3;
+}
+
